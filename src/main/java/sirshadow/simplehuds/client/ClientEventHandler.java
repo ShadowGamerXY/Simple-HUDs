4ea3f74af200015a23acf7cc091bd13ce32aaae0
sirshadow <shadowgamerxy@gmail.com>
2017-06-18 00:18:30 +0200

    changing

--- src/main/java/sirshadow/simplehuds/client/ClientEventHandler.java
+++ src/main/java/sirshadow/simplehuds/client/ClientEventHandler.java
@@ -2,15 +2,20 @@ package sirshadow.simplehuds.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import sirshadow.hudframework.client.hud.HUDElement;
+import sirshadow.hudframework.client.hud.HUDRenderHelper;
 import sirshadow.hudframework.client.util.RenderUtil;
 import sirshadow.simplehuds.ConfigurationHandler;
 import sirshadow.simplehuds.client.hud.HUDData;
+import sirshadow.simplehuds.client.hud.HUDExp;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by sirshadow on 6/5/17.
@@ -22,14 +27,14 @@ public class ClientEventHandler {
 
     @SubscribeEvent
     public void onGameOverlay(RenderGameOverlayEvent event) {
-        elementTypes.add(RenderGameOverlayEvent.ElementType.HEALTH);
-        elementTypes.add(RenderGameOverlayEvent.ElementType.FOOD);
-        elementTypes.add(RenderGameOverlayEvent.ElementType.EXPERIENCE);
-        elementTypes.add(RenderGameOverlayEvent.ElementType.AIR);
-        elementTypes.add(RenderGameOverlayEvent.ElementType.ARMOR);
+        addDisabledElement("HealthHUD",ElementType.HEALTH);
+        addDisabledElement("HungerHUD",ElementType.FOOD);
+        addDisabledElement("ExpHUD",ElementType.EXPERIENCE);
+        addDisabledElement("WaterHUD",ElementType.AIR);
+        addDisabledElement("ArmorHUD",ElementType.ARMOR);
 
 
-        if (event.getType().equals(RenderGameOverlayEvent.ElementType.TEXT)) {
+        if (event.getType().equals(ElementType.TEXT)) {
             if (ConfigurationHandler.fps_counter) {
                 int fps = Minecraft.getDebugFPS();
                 RenderUtil.addObject("FPS: " + fps, 0, 0, Color.WHITE);
@@ -46,9 +51,16 @@ public class ClientEventHandler {
     }
 
 
-    public void hudDataRender(){
-        for (int i = 0; i < HUDData.DATA.size();i++){
-
+    public void addDisabledElement(String blackListName,ElementType type){
+        for (Map.Entry<String,HUDElement>  entry : HUDRenderHelper.hudElementsMap.entrySet()) {
+            HUDElement element = entry.getValue();
+            if (element.getName().equalsIgnoreCase(blackListName)) {
+                if (!element.onBlacklist()) {
+                    if (!elementTypes.contains(type)) {
+                        elementTypes.add(type);
+                    }
+                } else elementTypes.remove(type);
+            }
         }
     }
 }
